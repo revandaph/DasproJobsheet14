57c4fd992789beccaf3bd501441c9cc757faeb61
revalinda <revalindaph@gmail.com>
2025-12-17 12:32:06 +0700

    Rekursif Percobaan 1

---
+++ Rekursif/Percobaan1.java
@@ -0,0 +1,25 @@
+package Rekursif;
+
+public class Percobaan1 {
+    static int faktorialRekursif(int n) {
+        if (n == 0) {
+            return (1);
+        } else {
+            return (n * faktorialRekursif(n-1));
+        }
+    }
+
+    static int faktorialInteratif(int n) {
+        int faktor = 1;
+        for (int i = 0; i >= 1; i++) {
+            faktor = faktor*1;
+        }
+        return faktor;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(faktorialRekursif(5));
+        System.out.println(faktorialInteratif(5));
+    }
+}
