3785e972ee936402f6802cfa566ab2f14f7e3d1c
revalinda <revalindaph@gmail.com>
2025-12-17 14:00:57 +0700

    Rekursif Percobaan 2

---
+++ Rekursif/Percobaan2.java
@@ -0,0 +1,25 @@
+package Rekursif;
+
+import java.util.Scanner;
+
+public class Percobaan2 {
+
+    static int HitungPangkat(int x, int y) {
+        if (y == 0) {
+            return (1);
+        } else {
+            return (x*HitungPangkat(x, y-1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Bilangan yang dihitung: ");
+        int bilangan = sc.nextInt();
+        System.out.print("Pangkat: ");
+        int pangkat = sc.nextInt();
+
+        System.out.println(HitungPangkat(bilangan, pangkat));
+    }
+}
