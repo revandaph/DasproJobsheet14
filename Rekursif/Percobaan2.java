de174ab8f426a88440258476826143a15e5edc57
revalinda <revalindaph@gmail.com>
2025-12-17 14:18:36 +0700

    Pertanyaan dan Modif Percobaan 2

--- Rekursif/Percobaan2.java
+++ Rekursif/Percobaan2.java
@@ -8,6 +8,7 @@ public class Percobaan2 {
         if (y == 0) {
             return (1);
         } else {
+            System.out.print(x + "x");
             return (x*HitungPangkat(x, y-1));
         }
     }
@@ -20,6 +21,26 @@ public class Percobaan2 {
         System.out.print("Pangkat: ");
         int pangkat = sc.nextInt();
 
-        System.out.println(HitungPangkat(bilangan, pangkat));
+        System.out.print ("Hasil: ");
+        int hasil= HitungPangkat(bilangan, pangkat);
+        System.out.println(" = " + hasil);
     }
 }
+
+/* PERTANYAAN PERCOBAAN 2
+
+1. Pada Percobaan2, terdapat pemanggilan fungsi rekursif hitungPangkat(bilangan, 
+pangkat) pada fungsi main, kemudian dilakukan pemanggilan fungsi hitungPangkat() 
+secara berulangkali. Jelaskan sampai kapan proses pemanggilan fungsi tersebut akan 
+dijalankan! 
+    = Pemanggilan fungsi rekursif HitungPangkat(x, y) akan terus dilakukan selama nilai y belum bernilai 0.
+    Setiap kali fungsi dipanggil, nilai y akan dikurangi satu (y - 1), sehingga proses perhitungan berjalan secara bertahap.
+    Pemanggilan fungsi akan berhenti ketika nilai y sama dengan 0. Pada kondisi ini, fungsi tidak lagi memanggil dirinya sendiri
+    dan langsung mengembalikan nilai 1. Kondisi tersebut disebut sebagai kondisi dasar (base case) yang berfungsi untuk menghentikan proses rekursi.
+    Jika kondisi dasar ini tidak dibuat, maka fungsi akan terus memanggil dirinya sendiri tanpa henti dan dapat menyebabkan kesalahan pada program,
+    seperti stack overflow.
+
+2. Tambahkan kode program untuk mencetak deret perhitungan pangkatnya. Contoh: 
+hitungPangkat(2,5) dicetak 2x2x2x2x2x1 = 32. Done
+
+*/
